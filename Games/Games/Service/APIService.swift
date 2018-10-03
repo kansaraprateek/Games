5108e836ba89ce26ebb98f7e0f25994185602201
Prateek Kansara <prateek@tagrem.com>
2018-10-03 15:24:23 +0530

    Initial Commit

---
+++ Games/Games/Service/APIService.swift
@@ -0,0 +1,9 @@
+//
+//  APIService.swift
+//  Games
+//
+//  Created by Prateek Kansara on 03/10/18.
+//  Copyright © 2018 Prateek. All rights reserved.
+//
+
+import Foundation
---
+++ Games/Games/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Games
+//
+//  Created by Prateek Kansara on 03/10/18.
+//  Copyright © 2018 Prateek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
